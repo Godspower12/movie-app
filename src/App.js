b8d2bf47aa9b9835684fe04269b87ad1f9af4e93
Godspower12 <66949234+Godspower12@users.noreply.github.com>
2022-04-14 15:25:08 +0100

    deploy to github pages

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import MyImage from "./images/image6.jpg"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1>My movie app</h1>
+     <h2>Author: Godspower</h2>
+     <img src= {MyImage} alt="profile-image"/>
     </div>
   );
 }
